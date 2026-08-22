567e1fe943c88b9c1a70cf437e75df7783b247fa
chesu0 <akfl06288@gmail.com>
2026-08-22 15:17:35 +0900

    3-06 회원가입 기능 구현

--- src/main/java/com/mysite/sbb/answer/AnswerController.java
+++ src/main/java/com/mysite/sbb/answer/AnswerController.java
@@ -26,11 +26,14 @@ public class AnswerController {
 			@Valid AnswerForm answerForm, BindingResult bindingResult) {
 		
 		Question question = this.questionService.getQuestion(id);
+		
 		if (bindingResult.hasErrors()) {
 			model.addAttribute("question", question);
 			return "question_detail";
 		}
+		
 		this.answerService.create(question, answerForm.getContent());
+		
 		return String.format("redirect:/question/detail/%s", id);
 	}
 
